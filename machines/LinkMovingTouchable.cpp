3f5b371d6423efd57aa815771400cccd8b87b3bf
Bracket <mcbracket@gmail.com>
2011-10-11 19:40:07 -0700

    separated LinkMovingTouchable into header and implementation files

---
+++ machines/LinkMovingTouchable.cpp
@@ -0,0 +1,37 @@
+#include <app_engine/ApplicationEngine.hpp>
+#include <machines/LinkMovingTouchable.hpp>
+#include <math/intersection.hpp>
+#include <math/Ray.hpp>
+#include <renderer/RenderingEngine.hpp>
+
+LinkMovingTouchable::~LinkMovingTouchable() {
+	ApplicationEngine::get()->erase_renderable(renderable_);
+	delete renderable_;
+}
+
+
+bool LinkMovingTouchable::handle_move_move(TouchHandler *, Vec2 const & loc) {
+	RenderingEngine * rendering_engine = ApplicationEngine::get()->get_rendering_engine();
+	Ray<3, float> ray = rendering_engine->unproject_device_independent(loc);
+
+	boost::optional<Vec3> pos = intersection(ray, Plane<3, float>(Vec3(0, 0, 1), 0));
+	if (!pos) { return false; }
+
+	renderable_->set_position(*pos);
+	return true;
+}
+
+bool LinkMovingTouchable::handle_move_end(TouchHandler * handler, Vec2 const & loc) {
+	handler->insert_rendezvous(loc, this);
+	return false;
+}
+
+void LinkMovingRenderable::render(RenderingEngine const * rendering_engine) {
+	square_.render(rendering_engine, get_frame());
+
+	if (touchable_) {
+		touchable_->set_bounding_rectangle(
+			square_.get_bounding_rectangle(rendering_engine, get_frame())
+		);
+	}
+}
--- machines/LinkMovingTouchable.hpp
+++ machines/LinkMovingTouchable.hpp
@@ -1,15 +1,11 @@
 #pragma once
 
-#include <app_engine/ApplicationEngine.hpp>
 #include <input/Touchable.hpp>
-#include <math/Ray.hpp>
 #include <renderer/Renderable.hpp>
-#include <renderer/RenderingEngine.hpp>
 #include <Square.hpp>
-#include <math/intersection.hpp>
 
-#include <iostream>
-
+class ApplicationEngine;
+class RenderingEngine;
 class LinkMovingTouchable;
 class LinkMovingRenderable;
 
@@ -45,39 +41,10 @@ class LinkMovingRenderable : public Renderable {
 
 		void set_touchable(LinkMovingTouchable * touchable) { touchable_ = touchable; }
 
-		void render(RenderingEngine const * rendering_engine) {
-			square_.render(rendering_engine, get_frame());
-
-			if (touchable_) {
-				touchable_->set_bounding_rectangle(
-					square_.get_bounding_rectangle(rendering_engine, get_frame())
-				);
-			}
-		}
+		void render(RenderingEngine const * rendering_engine);
 
 	private:
 		Square square_;
 		LinkMovingTouchable * touchable_;
 };
 
-inline LinkMovingTouchable::~LinkMovingTouchable() {
-	ApplicationEngine::get()->erase_renderable(renderable_);
-	delete renderable_;
-}
-
-
-inline bool LinkMovingTouchable::handle_move_move(TouchHandler *, Vec2 const & loc) {
-	RenderingEngine * rendering_engine = ApplicationEngine::get()->get_rendering_engine();
-	Ray<3, float> ray = rendering_engine->unproject_device_independent(loc);
-
-	boost::optional<Vec3> pos = intersection(ray, Plane<3, float>(Vec3(0, 0, 1), 0));
-	if (!pos) { return false; }
-
-	renderable_->set_position(*pos);
-	return true;
-}
-
-inline bool LinkMovingTouchable::handle_move_end(TouchHandler * handler, Vec2 const & loc) {
-	handler->insert_rendezvous(loc, this);
-	return false;
-}
